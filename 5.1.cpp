54067fca1ded3ca4ffdc25b9922e7e49a0cdb99f
s24287 <92215062+s24287@users.noreply.github.com>
2021-12-16 19:21:02 +0100

    Add files via upload

---
+++ 5.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Podaj rozmiar wektora: ";
+	cin >> n;
+
+	vector<int> ns(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cout << "Podaj wartosc dla ns[" << i << "]: ";
+		cin >> ns[i];
+	}
+
+	int max = 0;
+	int index = 0;
+	int i = 0;
+	for (const auto& v : ns)
+	{
+		if (v > max) {
+			max = v;
+			index = i;
+		}
+		i++;
+	}
+
+	cout << "najwiekszy element wektora to " << max << " o indeksie rownym " << index << endl;
+}
