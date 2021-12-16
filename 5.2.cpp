29e82ee9873cda0c0217ce1a93da90bb166f094c
s24287 <92215062+s24287@users.noreply.github.com>
2021-12-16 19:30:11 +0100

    Add files via upload

---
+++ 5.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Podaj rozmiar tablicy: ";
+	cin >> n;
+
+	int* tablica = new int[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cout << "Podaj wartosc dla tablicy [" << i << "]: ";
+		cin >> tablica[i];
+	}
+
+	for (int i = 0; i < n / 2; i++)
+	{
+		int X =  n;
+		int Y =  n--;
+
+		int tmp = tablica[X];
+		tablica[X] = tablica[Y];
+		tablica[Y] = tmp;
+	}
+}
